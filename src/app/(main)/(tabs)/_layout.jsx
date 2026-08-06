fced3542ff7a5f16d6460f50ece63f5209224ea7
Ankit chauhan <ankitch200@gmail.com>
2026-08-06 10:19:33 +0530

    style add

--- src/app/(main)/(tabs)/_layout.jsx
+++ src/app/(main)/(tabs)/_layout.jsx
@@ -7,6 +7,7 @@ export default function TabLayout() {
       screenOptions={{
         headerShown: false,
       }}
+      screenContentStyle={styles.screenContent}
     >
       <Tabs.Screen
         name="(home)"
