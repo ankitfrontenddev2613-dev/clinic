a9e61055ae1e5c5427f3973a900bb9012dffda77
Ankit chauhan <ankitch200@gmail.com>
2026-08-19 00:23:35 +0530

    store storage.js

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,3 +1,14 @@
-export const saveUser = async () => {
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+export const saveUser = async () => {
+  await AsyncStorage.setItem('user', JSON.stringify(user))
+}
+
+export const getUser = async () => {
+  await AsyncStorage.getItem('user');
+  return user ? JSON.parse(user) : null
+}
+
+export const removeUser = async () => {
+  await AsyncStorage.removeItem('user')
 }
